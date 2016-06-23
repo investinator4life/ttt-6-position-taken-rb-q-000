9df0a8430b967544cf8d73415fae66ebb58a4884
Mai Vong <mai.ly.vong@gmail.com>
2016-06-23 19:57:22 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,9 +2,7 @@
 def position_taken?(array, index)
   if array[index] == " " || array[index] == "" || array[index] == nil
     false
-  elsif array[index] == "X" || array[index] == "x"
+  else
     true
-  elsif array[index] == "O" || array[index] == "o"
-    true      
   end
 end
